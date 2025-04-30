b1fdcb91799bb6b9518e824c575cad85265b6661
Karthick0529 <karthicknandha619@gmail.com>
2025-04-30 11:44:58 +0530

    updated addToCart button

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -53,7 +53,32 @@ export const AppContextProvider = ({ children }) => {
     }
     toast.success("Removed from cart");
     setCartItems(cartData);
-  };
+  }
+
+  // Get total cart count
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  }
+
+  // Get total cart Amount
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product)=> product._id === items);
+      if(cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  }
+
+
 
   useEffect(() => {
     fetchProducts();
@@ -75,6 +100,8 @@ export const AppContextProvider = ({ children }) => {
     cartItems,
     searchQuery,
     setSearchQuery,
+    getCartAmount,
+    getCartCount,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
