6e57049d2713fd7535612e0fecbc883f88b0bcfd
Karthick0529 <karthicknandha619@gmail.com>
2025-04-30 13:39:37 +0530

    updated

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -64,7 +64,7 @@ const ProductDetails = () => {
                 .fill("")
                 .map((_, i) =>(
                 
-                    <img src={ i < 4 ? assets.star_icon : assets.star_dull_icon} alt="" className="md:w-4 w-3.5" />
+                    <img key={i} src={ i < 4 ? assets.star_icon : assets.star_dull_icon} alt="" className="md:w-4 w-3.5" />
                   
                 ))}
               <p className="text-base ml-2">(4)</p>
