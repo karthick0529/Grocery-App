984f11b17d7a906c2b67d47109766e8f9815e1d0
Karthick0529 <karthicknandha619@gmail.com>
2025-05-13 14:36:48 +0530

    seller pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 const App = () => {
 
@@ -36,9 +39,14 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />  
-          <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />} />
-          
-            
+          <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />} >
+
+            <Route index element={isSeller ? <AddProduct />: null} />
+            <Route path="product-list" element={isSeller ? <ProductList />: null} />
+            <Route path="orders" element={isSeller ? <Orders />: null} />
+
+
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
