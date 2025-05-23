046054f99e5201aa996e2605411a7e4da720d68a
Karthick0529 <karthicknandha619@gmail.com>
2025-05-23 13:07:25 +0530

    backend API's completed

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -19,4 +19,16 @@ export const addAddress = async (req, res) => {
 
 // Get Address : /api/address/get
 
-
+export const getAddress = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const addresses = await Address.find({ userId });
+        res.json({ success: true, addresses });
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import sellerRouter from "./routes/sellerRoute.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,7 +29,8 @@ app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
-
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 
 app.listen(port, () => {
