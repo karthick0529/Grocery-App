fd6e84596bc73f046f2efc4e20cce479ac9a191d
Karthick0529 <karthicknandha619@gmail.com>
2025-05-27 11:24:10 +0530

    updated frontend url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allowed origins for CORS
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://grocery-app-frontend-tawny.vercel.app"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
